a09b2135eedfe5aa987438eb5fc9f62be77cc855
vitkrv <vitkrv@gmail.com>
2017-07-17 21:32:45 +0300

    Add Bootstrap 4 and migrate to SCSS

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
 
