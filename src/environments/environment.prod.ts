4a6cf175fb078ebc7a8110518f29696a2533bf52
Vitaliy Kravchenko <vitaliy.kravchenko@sigma.software>
2017-08-07 15:02:34 +0300

    Update environment config; Update travis.yml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA9seut8nrnu_KIXD3SpvM3ZSoKuZCUMLI',
+    authDomain: 'shared-todo-9b23a.firebaseapp.com',
+    databaseURL: 'https://shared-todo-9b23a.firebaseio.com',
+    projectId: 'shared-todo-9b23a',
+    storageBucket: '',
+    messagingSenderId: '840781808105'
+  }
 };
